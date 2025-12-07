d32e1dd49b69a808e45beda02ab8061237557821
Jeet <jeetnath2110@gmail.com>
2025-12-07 14:36:31 +0530

    Responsive changes

--- frontend/src/components/partials/homeparts/Homefeaturesparts.jsx
+++ frontend/src/components/partials/homeparts/Homefeaturesparts.jsx
@@ -4,21 +4,21 @@ const Homefeaturesparts = () => {
   const SlickData = [
     {
       id: 1,
-      icon: <i class="ri-medicine-bottle-line"></i>,
+      icon: <i className="ri-medicine-bottle-line"></i>,
       title: "Easily book your medical appointment anytime, anywhere.",
       description:
         "We warmly greet every patient and strive to make their experience positive and comfortable from start to finish.",
     },
     {
       id: 2,
-      icon: <i class="ri-golf-ball-line"></i>,
+      icon: <i className="ri-golf-ball-line"></i>,
       title: "Meet Your Doctor and Discuss Your Health Concerns in Detail",
       description:
         "We warmly greet every patient and strive to make their experience positive and comfortable from start to finish.",
     },
     {
       id: 3,
-      icon: <i class="ri-syringe-line"></i>,
+      icon: <i className="ri-syringe-line"></i>,
       title: "Receive a Personalized Health Plan Tailored to Your Needs",
       description:
         "We warmly greet every patient and strive to make their experience positive and comfortable from start to finish.",
@@ -33,7 +33,7 @@ const Homefeaturesparts = () => {
   ];
   return (
     <section className="px-5 py-20 bg-blue-900 rounded-xl">
-      <div className="flex flex-row items-center text-white">
+      <div className="flex lg:flex-row flex-col items-center text-white">
         <div className="flex flex-col gap-3 lg:w-190 w-full">
           <p className="capitalize font-medium text-xl">
             How We Work to Put Your Health First
@@ -47,17 +47,15 @@ const Homefeaturesparts = () => {
           venenatis lacus eget nisi sodales, eget suscipit quam sollicitudin.
         </p>
       </div>
-      <div className="grid lg:grid-col-4 grid-cols-1">
         <Slick
-          baseclass="mt-14"
+          baseclassName="mt-18"
           dots={false}
           scroll={1}
           show={4}
-          cardclass={`bg-transparent px-6 py-14 rounded-xl shadow-sm border border-blue-700 text-white flex flex-col gap-5 hover:bg-white duration-300 ease-in-out hover:text-black transition-all cursor-pointer`}
+          cardclassName={`bg-transparent px-6 py-14 rounded-xl shadow-sm border border-blue-700 text-white flex flex-col gap-5 hover:bg-white duration-300 ease-in-out hover:text-black transition-all cursor-pointer`}
           carddata={SlickData}
           autoplay={true}
         />
-      </div>
     </section>
   );
 };

5c2d6290dcac4c669f1659baaa814fea8ccbe31a
Jeet <jeetnath2110@gmail.com>
2026-04-01 22:47:37 +0530

    responsive level changes

--- frontend/src/components/partials/common/Commonbannersection.jsx
+++ frontend/src/components/partials/common/Commonbannersection.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Commonbannersection = ({ title }) => {
   return (
-    <main className="h-70 w-full bg-blue-800 flex flex-col gap-4 items-center justify-center rounded-xl mb-4">
-      <h1 className="text-white text-6xl tracking-wide font-bold">{title}</h1>
+    <main className="lg:h-70 h-50 w-full bg-blue-800 flex flex-col lg:gap-4 gap-2 items-center justify-center lg:rounded-xl lg:mb-4">
+      <h1 className="text-white lg:text-6xl text-3xl tracking-wide font-bold">{title}</h1>
       <div className="flex flex-row gap-2">
         <Link to="/" className="text-white text-base font-medium">
           Home

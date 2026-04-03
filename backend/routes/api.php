cbdf10c0cbd5087d3516d9652454ff6500cf5353
Jeet <jeetnath2110@gmail.com>
2026-04-03 09:58:20 +0530

    test commit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,7 +18,7 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('home')->controller(AuthmanagementController::class)->group(function () {
         Route::middleware('auth:api')->group(function () {
-            Route::get('/', 'home');
+            Route::get('/home', 'home');
         });
     });
 });

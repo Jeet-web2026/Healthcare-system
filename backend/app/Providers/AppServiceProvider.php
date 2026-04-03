3b5bb1146cdd778995c548eab71344da41d122c9
Jeet <jeetnath2110@gmail.com>
2026-04-03 13:48:22 +0530

    Dependency injection resolved

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,13 +2,25 @@
 
 namespace App\Providers;
 
+use App\Repository\Eloquent\AboutusRepository;
 use App\Repository\Eloquent\HomeRepository;
+use App\Repository\Eloquent\OrganisationDetailsRepository;
 use App\Repository\Eloquent\UserManagementRepository;
+use App\Repository\Eloquent\WhoweareRepository;
+use App\Repository\Interface\AboutusRepositoryInterface;
+use App\Repository\Interface\AboutusServiceInterface;
 use App\Repository\Interface\AuthenticationServiceInterface;
 use App\Repository\Interface\HomeServiceInterface;
 use App\Repository\Interface\HomeServiceRepositoryInterface;
+use App\Repository\Interface\OrganisationDetailsManagementServiceInterface;
+use App\Repository\Interface\OrganisationDetailsRepositoryInterface;
 use App\Repository\Interface\UserManagementRepositoryInterface;
+use App\Repository\Interface\WhoweareRepositoryInterface;
+use App\Repository\Interface\WhoweareServiceInterface;
+use App\Services\Pages\AboutusService;
 use App\Services\Pages\HomeService;
+use App\Services\Pages\OrganisationDetailsManagementService;
+use App\Services\Pages\WhoweareService;
 use App\Services\User\AuthService;
 use Illuminate\Support\ServiceProvider;
 
@@ -40,6 +52,36 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             HomeServiceRepositoryInterface::class,
             HomeRepository::class
+        );
+
+        $this->app->bind(
+            AboutusServiceInterface::class,
+            AboutusService::class
+        );
+
+        $this->app->bind(
+            AboutusRepositoryInterface::class,
+            AboutusRepository::class
+        );
+
+        $this->app->bind(
+            OrganisationDetailsManagementServiceInterface::class,
+            OrganisationDetailsManagementService::class
+        );
+
+        $this->app->bind(
+            OrganisationDetailsRepositoryInterface::class,
+            OrganisationDetailsRepository::class
+        );
+
+        $this->app->bind(
+            WhoweareServiceInterface::class,
+            WhoweareService::class
+        );
+
+        $this->app->bind(
+            WhoweareRepositoryInterface::class,
+            WhoweareRepository::class
         );
     }
 }

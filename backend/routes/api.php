9ee731f1900b06bfe5f595e35f79ea883904f710
Jeet <jeetnath2110@gmail.com>
2026-04-03 09:54:13 +0530

    add gitignore for file pass

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,4 +15,10 @@ Route::prefix('v1')->group(function () {
             });
         });
     });
+
+    Route::prefix('home')->controller(AuthmanagementController::class)->group(function () {
+        Route::middleware('auth:api')->group(function () {
+            Route::get('/', 'home');
+        });
+    });
 });
